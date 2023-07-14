0ce25600b9a4d0576c64b3481429ca2d80a9f93e
Aaditya Mohan <aadimohan51@gmail.com>
2023-07-14 17:13:44 +0530

    project Structure updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar/navbar.component';
+import { EmployeeComponent } from './components/employee/employee/employee.component';
+import { ManagerComponent } from './components/manager/manager/manager.component';
+import { HrComponent } from './components/hr/hr/hr.component';
+import { SidebarComponent } from './sidebar/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    EmployeeComponent,
+    ManagerComponent,
+    HrComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
