e8f9b6abc546f510ef9165b7b023afbc484b66d3
Aaditya Mohan <aadimohan51@gmail.com>
2023-07-14 19:09:45 +0530

    navbar and sidebar added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar/navbar.component';
 
 const routes: Routes = [
-  {path : "", component : NavbarComponent},
+  
 ];
 
 @NgModule({
