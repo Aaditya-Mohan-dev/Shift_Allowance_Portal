dd3d11001ae6aa4be6250f0b9f8ff1129fc0e8a4
Aaditya Mohan <aadimohan51@gmail.com>
2023-07-15 11:47:45 +0530

    login added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ManagerRecordComponent } from './components/manager/manager-record/manager-record/manager-record.component';
 import { ManagerRequestComponent } from './components/manager/manager-request/manager-request/manager-request.component';
 import { EmployeeRequestComponent } from './components/employee/employee-request/employee-request/employee-request.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   {path : "manager-record",component:ManagerRecordComponent},
   {path: "manager-request",component:ManagerRequestComponent},
-  {path:"employee-request",component:EmployeeRequestComponent}
+  {path:"employee-request",component:EmployeeRequestComponent},
+  {path:"login",component:LoginComponent}
 ];
 
 @NgModule({
